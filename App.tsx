b2e432439e06c7cdf64fe681216699921a65aca3
death14stroke <mistry14darren@gmail.com>
2021-08-08 11:27:35 +0530

    Promotional notification android

--- App.tsx
+++ App.tsx
@@ -11,12 +11,14 @@ import {
 	Nunito_800ExtraBold,
 	Nunito_900Black
 } from '@expo-google-fonts/nunito';
+import messaging from '@react-native-firebase/messaging';
 import { NavigationProvider } from '@app/navigation';
 import { ProfileProvider } from '@app/context';
 
 // Android: Setting a timer for multiple minutes warning for firebase
 // expo-constants warning
 import { LogBox } from 'react-native';
+import { useEffect } from 'react';
 LogBox.ignoreLogs(['Setting a timer', 'Constants.']);
 
 const queryClient = new QueryClient();
@@ -32,6 +34,21 @@ const App: FC = () => {
 		Nunito_800ExtraBold,
 		Nunito_900Black
 	});
+
+	const requestUserPermission = async () => {
+		const authStatus = await messaging().requestPermission();
+		const enabled =
+			authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+			authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+		if (enabled) {
+			console.log('FCM authorization status:', authStatus);
+		}
+	};
+
+	useEffect(() => {
+		requestUserPermission();
+	}, []);
 
 	if (!fontsLoaded) {
 		return null;
